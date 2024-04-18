0cd5661661f306d60b1f974b6789fc51eec87da2
Marco Lamina <mlamina09@gmail.com>
2024-04-18 11:57:38 -0700

    Update PR creation handler

--- pr-creation-handler/pr-creation-handler.py
+++ pr-creation-handler/pr-creation-handler.py
@@ -3,14 +3,15 @@ import os
 from pr_pilot.util import create_task, wait_for_result
 
 pr_number = os.getenv("PR_NUMBER")
-review_instructions = os.getenv("REVIEW_INSTRUCTIONS")
+agent_instructions = os.getenv("AGENT_INSTRUCTIONS")
 repo = os.getenv("GITHUB_REPOSITORY")
 
 prompt = f"""
 The following PR was created: #{pr_number}
 
-Read the PR, then review it according to the following instructions:
-{review_instructions}
+Read the PR, then handle it according to the following instructions:
+
+{agent_instructions}
 """
 print(prompt)
 print(wait_for_result(create_task(repo, prompt, pr_number=pr_number)))
