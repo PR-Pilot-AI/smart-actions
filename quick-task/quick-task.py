2b4fd59d23cf1e7607cdb2551e7b364235c770fe
Marco Lamina <mlamina09@gmail.com>
2024-05-03 18:06:10 -0700

    Add PR number to task creation in quick-task

--- quick-task/quick-task.py
+++ quick-task/quick-task.py
@@ -4,6 +4,7 @@ from pr_pilot.util import create_task, wait_for_result
 
 repo = os.getenv("GITHUB_REPOSITORY")
 agent_instructions = os.getenv("AGENT_INSTRUCTIONS")
+pr_number = os.getenv("PR_NUMBER")
 
 print(agent_instructions)
-print(wait_for_result(create_task(repo, agent_instructions)))
+print(wait_for_result(create_task(repo, agent_instructions, pr_number=pr_number)))
